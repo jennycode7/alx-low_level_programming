828427be048cdeafcff2e202a94271ca4bd7a117
jennycode7 <jennessecosmas7@gmail.com>
2023-02-16 14:54:32 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	fprintf
+		(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
