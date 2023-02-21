fdbcfd1a013a42d2a579444fd7e6c3bc1f868ea7
jennycode7 <jennessecosmas7@gmail.com>
2023-02-21 10:07:25 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints 24 hours
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int x, i;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (i = 0; i < 60; i++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(':');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+		}
+	}
+}
