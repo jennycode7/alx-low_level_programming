f1fc83bc87d25e621ce4f7a2e8ace0534085139e
jennycode7 <jennessecosmas7@gmail.com>
2023-02-23 11:43:58 -0800

    fixing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,9 @@ void print_most_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if ((x != 2) || (x != 4))
+		if ((x == 2) || (x == 4))
+			continue;
+		else
 			putchar(x + '0');
 		putchar('\n');
 	}
