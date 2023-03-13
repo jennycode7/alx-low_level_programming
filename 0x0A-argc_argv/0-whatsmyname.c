c17913950faee52d45343adb1e6ee6adafa4e1b9
jennycode7 <jennessecosmas7@gmail.com>
2023-03-13 08:17:17 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: represents argument count
+ * @argv: represents arguments vector
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
