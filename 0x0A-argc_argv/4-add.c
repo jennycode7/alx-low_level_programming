5bb8c1de42c9f289770e006b579bf44dc4ddc996
jennycode7 <jennessecosmas7@gmail.com>
2023-03-14 03:41:06 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 /**
@@ -18,7 +19,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		if (isdigit(argv[i]) == 0)
 		{
 			printf("%s\n", "Error");
 			return (1);
