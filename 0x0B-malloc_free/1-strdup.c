76ad206b03357e7a3421a184ef7b28c87a567e15
jennycode7 <jennessecosmas7@gmail.com>
2023-03-18 07:15:21 -0700

    review task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -31,22 +31,3 @@ char *_strdup(char *str)
 
 	return (ptr);
 }
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
