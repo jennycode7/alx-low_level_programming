872f530d462b2ff030431e330ebec570d42817fb
jennycode7 <jennessecosmas7@gmail.com>
2023-03-22 16:52:44 -0700

    3-main.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,17 +1,15 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC
+#define CALC
 /**
  * struct op - Struct op
- *
  * @op: The operator
- * @f: The function associated
+ * @f: The function of that operator
  */
 typedef struct op
 {
 	char *op;
-	int (*f)(int a, int b);
+	int (*f)(int, int);
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,33 +1,73 @@
-#include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
-int (*get_op_func(char *s))(int, int)
+ * op_add - Return sum of a & b
+ * @a: First int to add
+ * @b: Second int to add
+ *
+ * Return: Sum of a & b
+ */
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (i < 5)
+/**
+ * op_sub - Return difference of a & b
+ * @a: First int to sub
+ * @b: Second itn to sub
+ *
+ * Return: Difference of a & b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Return product of a & b
+ * @a: First int to multiply
+ * @b: Second int to multiply
+ *
+ * Return: Product of a & b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Return quotient of a & b
+ * @a: Int to divide
+ * @b: Int to divide by
+ *
+ * Return: Quotient of a & b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
+	return (a / b);
+}
 
-	return (0);
+/**
+ * op_mod - Return remainder of a & b
+ * @a: Int to mod
+ * @b: Int to mod by
+ *
+ * Return: Remainder of a & b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
 
