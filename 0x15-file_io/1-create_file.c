840327f5f9b076717a3c308e1290f4339d40ee2a
jennycode7 <jennessecosmas7@gmail.com>
2023-05-11 15:29:01 -0700

    file i/o 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include <string.h>
 
 /**
-  * create_file - function that creates a file
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
-
+ * create_file - function that creates file
+ * @filename: filename
+ * @text_content: text to append to file
+ * Return: returns 1 or -1
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file_t;
+	int x;
 
 	if (!filename)
+	{
 		return (-1);
+	}
 
-	file_t = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_t == -1)
+	x = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (x == -1)
+	{
 		return (-1);
+	}
 
-	if (text_content)
-		write(file_t, text_content, strlen(text_content));
+	write(x, text_content, strlen(text_content));
 
-	close(file_t);
+	close(x);
 	return (1);
 }
