1e12a9d03c99fdb41484e2a20c86403e3fc4bbb1
root <jennycode7>
2023-06-07 17:16:19 -0700

    palindrome checker

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,7 +19,13 @@ int is_palindrome(char *s)
 	return (checker(s, 0, len - 1));
 }
 
-
+/**
+ * checker - supplementary function
+ * @s: string
+ * @start: to get first character
+ * @end: to get last character
+ * Return: returns 1 or 0
+ */
 int checker(char *s, int start, int end)
 {
 	if (start >= end)
@@ -30,7 +36,6 @@ int checker(char *s, int start, int end)
 	{
 		return (checker(s, start + 1, end - 1));
 	}
-	
 	else
 	{
 		return (0);
