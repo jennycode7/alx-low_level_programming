33c4a6ba33f1901403a886e5e361989fd25bfa2d
root <jennycode7>
2023-06-09 07:18:46 -0700

    number of vectors

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - write  number of vector
+ * @argc: number of vector
+ * @argv: vector
+ * Return: returns o
+ */
+int main(int argc, char **argv)
+{
+	argv[argc] = NULL;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
