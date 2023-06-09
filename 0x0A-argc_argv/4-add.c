7f0daaff9164eabe7c177d6e4dd8a2c599da28e9
root <jennycode7>
2023-06-09 10:36:57 -0700

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
 
+/**
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, r = 0, lim;
+
+	while (s1[a])
+	{
+		a++;
+	}
+
+	while (s2[b])
+	{
+		b++;
+	}
+
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
+}
+
+/**
+ * _atoi - converts string to integer
+ * @s: string
+ * Return: returns result
+ */
 int _atoi(char *s)
 {
 	int c = 0;
@@ -33,7 +87,12 @@ int _atoi(char *s)
 	return (ni);
 }
 
-
+/**
+ * main - entry point
+ * @argc: number of vectors
+ * @argv: vectors
+ * Return: returns 0
+ */
 int main(int argc, char **argv)
 {
 	int x = 0, y = 0, z = 0;
@@ -48,7 +107,7 @@ int main(int argc, char **argv)
 		for (x = 1; x < argc; x++)
 		{
 			z = _atoi(argv[x]);
-			if (z == 0 && argv[x] != 0)
+			if (z == 0 && _strcmp(argv[x], "0") != 0)
 			{
 				printf("Error\n");
 				return (1);
