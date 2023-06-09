33cd6ac789dbe0542dc0322f4bccd1f4712e6ffc
root <jennycode7>
2023-06-09 09:24:31 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+
+/**
+ * _atoi - converts string to integer
+ * @s: string
+ * Return: returns integer
+ */
+int _atoi(char *s)
+{
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
+	}
+
+	ni *= min;
+	return (ni);
+}
+
+/**
+ * main - entry point
+ * @argc: number of vectors
+ * @argv: vectors
+ * Return: returns 0 or -1
+ */
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	x = _atoi(argv[1]);
+	y = _atoi(argv[2]);
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	else
+	{
+		printf("%d\n", x * y);
+		return (0);
+	}
+}
