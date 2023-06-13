4dce71fc6e46ea9348a93e22ad165ab81edb784f
root <jennycode7>
2023-06-13 10:36:30 -0700

    duplication

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - function that duplicates string
+ * @str: string
+ * Return: returns duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *strcpy;
+	int i, len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	len = strlen(str);
+	strcpy = malloc(sizeof(char) * len + 1);
+
+	if (strcpy == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		strcpy[i] = str[i];
+	}
+
+	return (strcpy);
+}
