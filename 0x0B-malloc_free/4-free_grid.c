235ca62a8e9b46104a1c0850e8b3f269c8a6df13
root <jennycode7>
2023-06-13 17:25:00 -0700

    freer

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * free_grid - freees array
+ * @grid: array to be freed
+ * @height: ...
+ */
 void free_grid(int **grid, int height)
 {
-	while(height--)
+	while (height--)
 	{
 		free(grid[height]);
 	}
