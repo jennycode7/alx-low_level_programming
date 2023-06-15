625a1f0388cae7708c1d4294d2157dff9c0582fa
root <jennycode7>
2023-06-15 08:11:19 -0700

    concatenation

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates string
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes
+ * Return: return string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, x, y, z;
+	char *str;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		;
+	}
+	if (n >= i)
+	{
+		n = i;
+	}
+
+	y = strlen(s1);
+	str = malloc(sizeof(char) * y + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < y; x++)
+	{
+		str[x] = s1[x];
+	}
+	for (z = x, i = 0; z < x + n; z++)
+	{
+		str[z] = s2[i];
+		i++;
+	}
+	str[z] = '\0';
+
+	return (NULL);
+}
