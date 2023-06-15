1e22f2bd99c190a63c3f2b92f28f3c92d6338321
root <jennycode7>
2023-06-15 09:01:42 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * _calloc - allocates memory
+ * @nmemb: ...
+ * @size: size of bytes
+ * Return: returns pointer to memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *str;
+	unsigned int x;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	x = size;
+	str = malloc(nmemb * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	return (str);
+}
