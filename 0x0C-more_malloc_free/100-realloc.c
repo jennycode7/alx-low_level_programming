3a6feb5cf42d94512073f9905c3a289b87e032b2
root <jennycode7>
2023-06-16 12:51:58 -0700

    reallocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
 	str = malloc(new_size);
 
 	if (old_size < new_size)
