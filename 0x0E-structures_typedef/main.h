a5a7c955bdb68cec25af87099f15b868fcbf5245
root <jennycode7>
2023-06-19 04:08:35 -0700

    struct dog

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ * struct dog - a struct
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
