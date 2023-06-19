f81ff114104fef6bda49a93d8304bb2fe01b40db
root <jennycode7>
2023-06-19 13:50:25 -0700

    çreation

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int x, y;
+
+	if (name != NULL && owner != NULL)
+	{	
+		x = strlen(name) + 1;
+		y = strlen(owner) + 1;
+
+
+		dog = malloc(sizeof(dog_t));
+
+		if (dog == NULL)
+		{
+			return (NULL);
+		}
+
+
+		dog->name = malloc(sizeof(char) * x);
+		if (dog->name == NULL)
+		{
+			free(dog->name);
+			return (NULL);
+		}
+
+		dog->owner = malloc(sizeof(char) * y);
+		if (dog->owner == NULL)
+		{
+			free(dog->owner);
+			return (NULL);
+		}
+
+		dog->name = strcpy(dog->name, name);
+		dog->owner = strcpy(dog->owner, owner);
+		dog->age = age;
+	}
+	return (dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_H
+#ifndef  MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct dog - a struct
  * @name: ...
@@ -17,6 +18,6 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
