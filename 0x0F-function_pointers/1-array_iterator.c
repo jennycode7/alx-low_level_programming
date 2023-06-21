8acf73a799cb9ac2657a0b6b08c10c7c7bbd57ba
root <jennycode7>
2023-06-21 11:40:57 -0700

    pointers to array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints array
+ * @array: ...
+ * @size: ...
+ * @action: ...
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size <= 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+
+}
