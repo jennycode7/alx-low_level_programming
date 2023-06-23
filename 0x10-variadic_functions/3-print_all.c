65f488adcaef6b42c9371d35e2c877ec52930d0a
root <jennycode7>
2023-06-23 05:41:25 -0700

    printer 2

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,7 +31,7 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				str = va_arg(ap, char *);
-				str = str ? str : "(nill)";
+				str = str ? str : "(nil)";
 				printf("%s%s", str, space);
 				break;
 		}
