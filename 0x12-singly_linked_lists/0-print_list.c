823e75918f84d289fef70a1976c61792f84b0ffa
jennycode7 <jennessecosmas7@gmail.com>
2023-07-04 11:55:09 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_list
+ * @h: head
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+	int x;
+	const list_t *current = h;
+
+	for (n = 0; current; n++)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			x = strlen(current->str);
+			printf("[%d] %s\n", x, current->str);
+		}
+		current = current->next;
+	}
+
+	return (n);
+}
