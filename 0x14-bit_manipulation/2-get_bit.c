0de1aafa39ef0e18ab316106e9f86e9bb9af0887
jennycode7 <jennessecosmas7@gmail.com>
2023-07-08 07:48:53 -0700

    get bits

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * get_bit - get bits
+ * @n: ...
+ * @index: ...
+ * Return: bits
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int x;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (x = 0; x < index; x++)
+	{
+		n /= 2;
+	}
+
+	if ((n & 1) == 1)
+	{
+		return (1);
+	}
+
+	else if ((n & 1) == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (-1);
+	}
+}
