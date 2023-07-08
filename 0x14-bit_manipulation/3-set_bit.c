86b9dfd0a96b528000cf3dab498cd5f8b906cd85
jennycode7 <jennessecosmas7@gmail.com>
2023-07-08 14:29:53 -0700

    set bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * set_bit - sets bit
+ * @n: ...
+ * @index: ...
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n = *n | 1 << index;
+
+	return (1);
+}
