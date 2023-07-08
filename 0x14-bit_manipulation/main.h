afa329dd2ec4e3e3b12e6a13a6dc964a7c84efd9
jennycode7 <jennessecosmas7@gmail.com>
2023-07-08 14:40:12 -0700

    clear bits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,7 @@ void binary(unsigned long int n);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 
 #endif
