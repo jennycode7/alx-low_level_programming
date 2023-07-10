c2b45aec59d8cfa17a30196aa769b4fd8e94ebff
jennycode7 <jennessecosmas7@gmail.com>
2023-07-10 07:24:39 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 	{
 		return (0);
