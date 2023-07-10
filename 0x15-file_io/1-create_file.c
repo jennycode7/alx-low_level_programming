bb185716b84b4ead2894e9e97214d4965db949d6
jennycode7 <jennessecosmas7@gmail.com>
2023-07-10 15:04:31 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, S_IUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
