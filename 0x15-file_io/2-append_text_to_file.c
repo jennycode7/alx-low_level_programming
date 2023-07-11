a68d6d3cdf84dd7a96a83e91a23df57c5587780a
jennycode7 <jennessecosmas7@gmail.com>
2023-07-10 18:39:19 -0700

    appands

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file: appends
+ * @filename: ...
+ * @text_content: ...
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, x;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (textname)
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+		{
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
