c1b341a0191b2ffdd918c4dc721786278ab87dc3
jennycode7 <jennessecosmas7@gmail.com>
2023-08-07 18:13:02 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: ...
+ * @text_content: ...
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, y;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		y = write(fd, text_content, strlen(text_content));
+		if (y == -1)
+		{
+			return (-1);
+		}
+	}
+
+	return (0);
+}
