d945e47e6d647d9c8792cef1e939ce459ecfb7e6
jennycode7 <jennessecosmas7@gmail.com>
2023-08-08 15:41:53 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,10 +21,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	y = write(fd, text_content, strlen(text_content));
-	if (y == -1)
+	if (text_content != NULL)
 	{
-		return (-1);
+		y = write(fd, text_content, strlen(text_content));
+		if (y == -1)
+		{
+			return (-1);
+		}
 	}
 
 	close(fd);
