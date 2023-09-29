88fcbb49946958e6d1e83efde56ee413eaf7150f
jennycode7 <jennessecosmas7@gmail.com>
2023-09-28 20:11:57 -0700

    endianess

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * get_endianess - endianess
+ * get_endianness - endianess
  * Return: int
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
 
-	unsigned char *ptr = (unsigned char*)&num;
+	unsigned char *ptr = (unsigned char *)&num;
 
 
 	return (*ptr == 1);
