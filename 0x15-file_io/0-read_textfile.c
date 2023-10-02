745fb076289a2ce97bf335499da8b00348d9e690
jennycode7 <jennessecosmas7@gmail.com>
2023-10-02 14:53:49 -0700

    opens a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	store = malloc(sizeof(char) * letters);
+	store = malloc(sizeof(char *) * letters);
 	if (store == NULL)
 	{
 		return (0);
