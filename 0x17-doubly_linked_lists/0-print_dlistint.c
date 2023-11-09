74036c866056c7e58ac079309a7c44f4638be54b
jennycode7 <jennessecosmas7>
2023-11-09 05:29:19 -0800

    print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints double list
+ * @h: head
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int x = 0;
+	
+	if (!h)
+	{
+		return (0);
+	}
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x += 1;
+	}
+	return (x);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
