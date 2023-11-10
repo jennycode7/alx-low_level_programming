33f23d2d713a379c007240d0d52c03b3f334bc77
jennycode7 <jennessecosmas7>
2023-11-09 18:16:34 -0800

    adds node at end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *temp = *head;
-  
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
@@ -27,14 +27,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-   
+
 	while (temp->next)
 	{
 		temp = temp->next;
 	}
-    
+
 	temp->next = new_node;
 	new_node->prev = temp;
-  
+
 	return (new_node);
 }
